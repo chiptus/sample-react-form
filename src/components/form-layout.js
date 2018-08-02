9133fbdb53e64f1f56295963a9e479d3bc0e0f2f
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2018-08-02 15:07:25 +0100

    move steps to up

--- src/components/form-layout.js
+++ src/components/form-layout.js
@@ -3,8 +3,10 @@ import React from "react";
 import Steps from "./steps";
 import ProgressBar from "./progress-bar";
 
+import "./form-layout.css";
+
 export default ({ children, currentStep }) => (
-  <form>
+  <form class="form-layout">
     <ProgressBar currentStep={currentStep} />
     {children}
     <Steps currentStep={currentStep} />
--- src/components/progress-bar.js
+++ src/components/progress-bar.js
@@ -1,13 +1,20 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import "./progress-bar.css";
 
 const ProgresBar = ({ currentStep }) => (
   <div className="progress-bar">
-    <div className={`step ${currentStep === 1 ? "active" : ""}`}>Step 1</div>
-    <div className={`step ${currentStep === 2 ? "active" : ""}`}>Step 2</div>
-    <div className={`step ${currentStep === 3 ? "active" : ""}`}>Step 3</div>
+    <ul className="steps">
+      <li className={`step ${currentStep === 1 ? "active" : ""}`}>Step 1</li>
+      <li className={`step ${currentStep === 2 ? "active" : ""}`}>Step 2</li>
+      <li className={`step ${currentStep === 3 ? "active" : ""}`}>Step 3</li>
+    </ul>
+    <div className={`bar bar${((currentStep - 1) / 2) * 100}`} />
   </div>
 );
+
+ProgresBar.propTypes = {
+  currentStep: PropTypes.number.isRequired
+};
 
 export default ProgresBar;
